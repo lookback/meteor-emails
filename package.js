fc9db5b43185100127c9fa4651fc654deb4d7988
Johan Brook <johan.jvb@gmail.com>
2015-03-02 20:16:08 +0100

    Version 0.3.0

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ var where = 'server';
 Package.describe({
   name: 'lookback:emails',
   summary: 'Send emails with server side templates.',
-  version: '0.2.0'
+  version: '0.3.0'
 });
 
 Npm.depends({
