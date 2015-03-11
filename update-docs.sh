fb781f4e80175fa6c4f9bbe40c1072552cd1bb3f
Johan Brook <johan.jvb@gmail.com>
2015-03-11 20:47:46 +0100

    Generate docs in /docs dir

--- update-docs.sh
+++ update-docs.sh
@@ -3,7 +3,8 @@
 git stash && \
 git checkout gh-pages && \
 git rebase master && \
-docco emails.coffee utils.coffee -o . && \
+rm -rf docs && \
+docco emails.coffee utils.coffee && \
 git add . && \
 git commit -a -m 'Generate documentation' && \
 git push -f origin gh-pages && \
