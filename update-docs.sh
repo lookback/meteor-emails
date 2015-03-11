a6a346b1703daf8b0e47179d1a1e69159f2431e4
Johan Brook <johan.jvb@gmail.com>
2015-03-11 20:44:47 +0100

    Change name of docs.sh

--- update-docs.sh
+++ update-docs.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-git checkout -b gh-pages && \
+git stash && \
+git checkout gh-pages && \
 git rebase master && \
 docco emails.coffee utils.coffee -o . && \
 git add . && \
 git commit -a -m 'Generate documentation' && \
 git push -f origin gh-pages && \
 git checkout master && \
+git stash pop && \
 echo "Done: Generated documentation"
