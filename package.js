fd276129fe74c1c72dc8cb88c733d75fd23d064e
Johan Brook <johan.jvb@gmail.com>
2015-03-26 09:05:03 +0100

    Version 0.3.2

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ var where = 'server';
 Package.describe({
   name: 'lookback:emails',
   summary: 'Send HTML emails with server side Blaze templates. Preview and debug in the browser.',
-  version: '0.3.1',
+  version: '0.3.2',
   git: 'https://github.com/lookback/meteor-emails.git'
 });
 
