ca908c3f8f2e1e2bbe5c9375be73723e2df1d888
Johan Brook <johan.jvb@gmail.com>
2015-10-21 21:52:27 +0200

    Update docs script to delete the gh-pages branch when done

--- update-docs.sh
+++ update-docs.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 git stash && \
-git checkout gh-pages && \
-git rebase master && \
+git checkout -b gh-pages && \
 rm -rf docs && \
 rm -rf example && \
 docco emails.coffee utils.coffee && \
@@ -10,5 +9,6 @@ git add . && \
 git commit -a -m 'Generate documentation' && \
 git push -f origin gh-pages && \
 git checkout master && \
+git branch -D gh-pages && \
 git stash pop && \
 echo "Done: Generated documentation"
